0622836b1d3a6b68da646cd9713b52e2e68b175d
Wildnmr <53279332+Wildnmr@users.noreply.github.com>
2023-06-02 11:40:11 -0700

    Add files via upload

--- python/hello.py
+++ python/hello.py
@@ -3,6 +3,44 @@ print("hello world")
 print("add my name")
 
 
+#strip removes any leading and trailing spaces
+
+
+txt = "     banana     "   # notice the spaces
+
+x = txt.strip()     # strip all spaces
+
+print("of all fruits", x, "is my favorite")
+
+
+
+say = "This is my sentense to strip out"
+
+y = say.strip('s')
+
+print(y)
+
+txt1 = ",,,,,rrttgg.....banana....rrr"
+
+z = txt1.strip(",.grt")
+
+print(z)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
